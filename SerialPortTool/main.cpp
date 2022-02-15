d59b97eb49250378cf6a470d220ab6dc7c71e169
mgh <mghcool@163.com>
2022-02-15 23:39:08 +0800

    设置相对路径，添加文本框右键翻译

--- SerialPortTool/main.cpp
+++ SerialPortTool/main.cpp
@@ -1,9 +1,18 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include <QTranslator>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // 设置相对路径
+    QDir::setCurrent(a.applicationDirPath());
+    // 加载翻译文件
+    QTranslator translator;
+    translator.load("widgets_zh_cn.qm");
+    a.installTranslator(&translator);
+    // 打开窗口
     MainWindow w;
     w.show();
     return a.exec();
